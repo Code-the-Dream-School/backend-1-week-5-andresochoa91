f41245152acdee31a7fde6cba3e201e6279ea1db
andresochoa91 <andres_ochoa91@hotmail.com>
2020-09-21 11:06:52 -0700

    pig_latin added

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,18 @@
 #write your code here
+def translate(w)  
+
+  def validation(word) 
+    vowels = "aeiou"
+    consonants = "bcdfghjklmnpqrstvwxyz"
+    if vowels.include? word[0]
+      return "#{word}ay"
+    elsif consonants.include? word[0]
+      temp = word[0, 2].downcase == "qu" ? [2, word[0, 2]] : [1, word[0]]
+      validation("#{word[temp[0], (word.length - 1)]}#{temp[1]}")
+    end  
+  end
+  
+  w.split(" ").map{ |wo| validation(wo)}.join(" ")
+
+end
+puts translate("querer")
