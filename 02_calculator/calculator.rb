081b42015a3a24ea0b7025f17b526a88b365fa41
andresochoa91 <andres_ochoa91@hotmail.com>
2020-09-20 23:07:52 -0700

    updates

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,16 @@
 #write your code here
+def add(num1, num2)
+  num1 + num2
+end
+
+def subtract(num1, num2)
+  num1 - num2
+end
+
+def sum(arr)
+  arr.inject(0) { |acc, num| acc + num}
+end
+
+# def multiply()
+#   num1 * num2
+# end
