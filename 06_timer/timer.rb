1930a9a46d640e9c983eaf0df2259303bf050d63
andresochoa91 <andres_ochoa91@hotmail.com>
2020-09-21 13:52:31 -0700

    Assignment ready

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,15 @@
 class Timer
   #write your code here
+  attr_accessor :seconds
+  
+  def initialize(seconds=0)
+    @seconds = seconds
+  end
+  
+  def time_string
+    Time.at(@seconds.to_i.abs).utc.strftime "%H:%M:%S"
+  end
 end
+
+newtimer = Timer.new(12)
+puts newtimer.time_string
