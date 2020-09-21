c699ccabbd44457f25937a34896cb03c11706fb6
andresochoa91 <andres_ochoa91@hotmail.com>
2020-09-21 00:43:28 -0700

    Almost ready

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -11,6 +11,14 @@ def sum(arr)
   arr.inject(0) { |acc, num| acc + num}
 end
 
-# def multiply()
-#   num1 * num2
-# end
+def multiply(num1, num2, *others)
+  num1 * num2 * others.inject(1) { |acc, num| acc * num }
+end
+
+def power(num1, num2)
+  num1 ** num2
+end
+
+def factorial(num)
+  num <= 0 ? 0 : (1..num).inject(1){ |acc, n| acc * n}
+end
